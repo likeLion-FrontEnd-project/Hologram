f05925001064bd1c1ad1b7c3e16d1282ec0ea3d4
GitHWS <gitcodehw@gmail.com>
2022-07-14 14:22:47 +0900

    [Add] login.html, login.css 수정 & login.js 파일 추가 login.html, login.css 수정 및 login.js 파일 추가
    resolves: #118

---
+++ js/login.js
@@ -0,0 +1,52 @@
+const emailInput = document.querySelector('.email-input');
+const pwInput = document.querySelector('.password-input');
+const loginBtn = document.querySelector('.login-btn');
+const errMsg = document.querySelector('.error-message');
+
+function loginBtnActive() {
+  if (emailInput.value !== '' && pwInput.value !== '') {
+    loginBtn.disabled = false;
+    loginBtn.classList.add('active');
+  } else {
+    loginBtn.disabled = true;
+    loginBtn.classList.remove('active');
+  }
+}
+
+loginBtn.addEventListener('input', loginBtnActive);
+pwInput.addEventListener('input', loginBtnActive);
+
+async function loginData() {
+  const url = 'https://mandarin.api.weniv.co.kr';
+  try {
+    const res = await fetch(`${url}/user/login/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        user: {
+          email: emailInput.value,
+          password: pwInput.value,
+        },
+      }),
+    });
+    const resJson = await res.json();
+    console.log(resJson);
+
+    if (resJson.status !== 422) {
+      location.href = './feed.html';
+    } else {
+      errMsg.classList.add('error');
+      loginBtn.classList.remove('active');
+    }
+    localStorage.setItem('token', resJson.user.token);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+loginBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  loginData();
+});
