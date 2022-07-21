1ff1b8f619133e7617bc00fc1c45dd9fdbffd732
joodb <joodb@github.com>
2022-07-21 14:08:31 +0900

    [Add] 프로필 페이지 팔로우 버튼 기능
    프로필 페이지에서 팔로우 버튼 기능 추가
    다른 유저 팔로우 하고 있는 상태  -> 언팔로우 버튼
    다른 유저 언팔로우 하고 있는 상태 -> 팔로우 버튼
    
    resolves: #204

--- js/profile.js
+++ js/profile.js
@@ -41,7 +41,7 @@ cancelBtn.addEventListener('click', ()=>{
 })
 logoutBtn.addEventListener('click', ()=>{
   // 로그아웃
-  location.href='./splash.html####';
+  location.href='../pages/splash.html####';
 })
 
 
@@ -62,14 +62,19 @@ async function getUserProfile(accountname) {
       'Content-type': 'application/json'
     },
   }
+
+
   try{
     // userProfile
     const userProfileData = await fetch(`${url}/profile/${accountName}`, getUserData);
     const userProfileJson = await userProfileData.json();
     const userProfile = userProfileJson.profile;
-    setUserProfile(userProfile);
-    // 프로필 상태별 버튼
-    checkUser(userProfile);
+
+    const followingData = await fetch(`${url}/profile/${accountName}/follower`, getUserData);
+    const followList = await followingData.json();
+    const userFollowing = userProfile.following;
+    
+    setUserProfile(userProfile, followList);
     
     // userPost
     const userPostData = await fetch(`${url}/post/${accountName}/userpost`, getUserData);
@@ -82,44 +87,120 @@ async function getUserProfile(accountname) {
   }
 }
 
-/* 유저 프로필 정보 뿌려주기 */
-function setUserProfile(userProfile) {
+/* 팔로잉 리스트 받아오기  */
+// async function getFollowingList() {
+//   const token = localStorage.getItem('token');
 
-  const user_image = (userProfile.image) ? userProfile.image : '../assets/images/img-profile_large.png';
-  
-  document.querySelector('.follower-num').textContent = userProfile.followerCount;
-  document.querySelector('.following-num').textContent = userProfile.followingCount;
-  document.querySelector('.profile-img-wrap img').src = user_image;
-  document.querySelector('.profile-section .profile-name').textContent = userProfile.username;
-  document.querySelector('.profile-section .profile-account').textContent = `@ ${userProfile.accountname}`;
-  document.querySelector('.profile-section .profile-intro').textContent = userProfile.intro;
-}
+//   const getAccount = location.search.replace("?", "").split("=");
+//   const accountName = (getAccount == '') ?
+//     localStorage.getItem('accountname') : getAccount[1];
+
+//   const getFollowingData = {
+//     method: 'GET',
+//     headers: {
+//       'Authorization': `Bearer ${token}`,
+//       'Content-type': 'application/json'
+//     },
+//   }
+//   const followingData = await fetch(`${url}/profile/${accountName}/following`, getFollowingData);
+//   const followingList = await followingData.json();
+//   const userFollowing = userProfile.following;
+// }
 
 const myProfileBtnWrap = document.querySelector('.myProfile-btns');
 const followBtnWrap = document.querySelector('.follow-btns');
 const unfollowBtnWrap = document.querySelector('.unfollow-btns');
-/* 프로필 상태별 버튼 */
-function checkUser(userProfile) {
-  // userAccountname 현재 프로필 페이지에서 보이는 사용자
+
+/* 유저 프로필 정보 뿌려주기 */
+function setUserProfile(userProfile, followList) {
+  console.log('현재의 유저'); 
+  console.log(userProfile);
+  console.log(followList); // 로그인한 나의 팔로잉 리스트
+  // 팔로우 리스트에 내 아이디가 있다면?
+
+
+  const user_image = (userProfile.image) ? userProfile.image : '../assets/images/img-profile_large.png';
+  
+  document.querySelector('.follower-num').textContent = userProfile.followerCount;
+  document.querySelector('.follower-wrap').setAttribute('href', `/pages/followers.html?accountname=${userProfile.accountname}`);
+  document.querySelector('.following-num').textContent = userProfile.followingCount;
+  document.querySelector('.following-wrap').setAttribute('href', `/pages/followings.html?accountname=${userProfile.accountname}`);
+  document.querySelector('.profile-img-wrap img').src = user_image;
+  document.querySelector('.profile-section .profile-name').textContent = userProfile.username;
+  document.querySelector('.profile-section .profile-account').textContent = `@ ${userProfile.accountname}`;
+  document.querySelector('.profile-section .profile-intro').textContent = userProfile.intro;
+  
+  const localUser = localStorage.getItem('accountname');
+  const nowUser = userProfile.accountname;
+  console.log(userProfile.isfollow);
+  
+  if (nowUser === localUser) { // 내 프로필일 때
+    myProfileBtnWrap.classList.add('on');
+  } 
+  else if (userProfile.isfollow === false) { // 팔로우 중이 아니라면
+    followBtnWrap.style.display = 'flex';
+    followBtn.textContent = '팔로우';
+  } else if (userProfile.isfollow === true ) { // 팔로우 중이라면
+    followBtnWrap.style.display = 'flex';
+    followBtn.classList.replace('user-follow-btn', 'user-unfollow-btn');
+    followBtn.textContent = '언팔로우';
+  }
+}
+
+// follow-btn -> profile-unfollow-btn
+
+// follow, unfollow 상태 변경
+async function changeFollow(userProfile){
   const localUser = localStorage.getItem('accountname');
   const nowUser = userProfile.accountname;
   
-  if (nowUser === localUser) { // 내 프로필일 때
-    myProfileBtnWrap.style.display = 'flex';
-  } else if (userProfile.isfollow == false) { // 팔로우 중이 아니라면
-    followBtnWrap.style.display = 'flex';
-  } else if (userProfile.isfollow == true ) { // 팔로우 중이라면
-    unfollowBtnWrap.style.display = 'flex';
+  const token = localStorage.getItem('token');
+  const getAccount = location.search.replace("?","").split("=");
+  const accountName = (getAccount == '') ? 
+    localStorage.getItem('accountname') : getAccount[1];
+  
+  // 팔로우 됨
+  if(followBtn.classList.contains('user-follow-btn')){ // 현재 팔로우 중임 
+    try {
+      const resFollow = await fetch(`${url}/profile/${accountName}/follow`, 
+      {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-type': 'application/json',
+        },
+      });
+      const resFollowJson = await resFollow.json();
+    }catch(err){
+      console.error('err', err);
+    }
+    console.log('팔로우 성공');
+    followBtn.classList.replace('user-follow-btn', 'user-unfollow-btn');
+    followBtn.textContent = '언팔로우';
+    
+  }else if(followBtn.classList.contains('user-unfollow-btn')){ 
+    try {
+      const resUnfollow = await fetch(`${url}/profile/${accountName}/unfollow`,
+        {
+          method: 'DELETE',
+          headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-type': 'application/json',
+          },
+        });
+        const resUnfollowJson = await resUnfollow.json();
+        console.log('이제 님 언팔함');
+        console.log(resUnfollowJson);
+    }catch(err){
+      console.error('err', err);
+    }
+    followBtn.classList.replace('user-unfollow-btn', 'user-follow-btn');
+    followBtn.textContent = '팔로우';
   }
-} 
-/* 프로필 상태별 페이지 이동 */
-const chatBtn = document.querySelector('#follow-chat-btn')
-const followingChatBtn = document.querySelector('#following-chat-btn')
-function moveChat() {
-  location.href='./chat.html';
 }
-chatBtn.addEventListener('click', moveChat)
-followingChatBtn.addEventListener('click', moveChat)
+const followBtn = document.querySelector('.user-follow-btn');
+followBtn.addEventListener('click', changeFollow);
+
 
 
 /* 게시글 - 리스트, 앨범 */
