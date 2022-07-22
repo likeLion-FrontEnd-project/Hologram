b9b149803794081e910fd96052e9dbe63a3435ec
joodb <joodb@github.com>
2022-07-22 10:20:47 +0900

    [Add] 상품명, 가격 유효성 검사
    상품명 글자 수 제한 에러메세지
    상품가격 숫자 입력, 원화 단위 콤마
    
    resolves: #185

--- js/product.js
+++ js/product.js
@@ -1,5 +1,10 @@
 const productImg = document.querySelector('#product-img');
 let filename = '';
+const productName = document.querySelector('#product-name');
+const productPrice = document.querySelector('#product-price');
+
+productName.addEventListener('input', checkName);
+productPrice.addEventListener('input', checkPrice);
 
 // 이미지 업로드 시 미리보기
 async function productImgShow(event) {
@@ -29,3 +34,23 @@ async function productImgUpload(target) {
     console.error(err);
   }
 };
+
+// 이름 유효성 검사
+function checkName() {
+  const errorMsg = document.querySelector(".error-msg");
+  // 글자수에 맞는지 체크
+  if (productName.value.length < 2 || productName.value.length > 16) {
+    errorMsg.style.display = 'block';
+    productName.classList.add("error");
+    // form 제출안되게 하기 
+  } else {
+    errorMsg.style.display = 'none'
+    productName.classList.remove("error");
+  }
+}
+
+// 가격 - 숫자 입력, 콤마
+function checkPrice (e) {
+  let resultPrice = e.target.value.replace(/[^0-9]/g,'');
+  e.target.value = resultPrice.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
