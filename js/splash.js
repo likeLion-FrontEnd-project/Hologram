664cb3cafe54f7444268d543f0785ec497acac0e
GitHWS <gitcodehw@gmail.com>
2022-07-25 21:47:32 +0900

    [Add] splash 페이지 로드 후 localstorage에 token이 존재 시 feed 페이지로 이동하는 기능 구현 splash 페이지에서 이미 token을 가지고 있는 로그인 상태라면 feed 페이지로 이동
    resolves: #254

--- js/splash.js
+++ js/splash.js
@@ -1,5 +1,8 @@
 // 1초 후 지정한 경로로 페이지 이동
-
 window.setTimeout(() => {
-  location.href = '/pages/intro.html';
+  if (localStorage.getItem('token')) {
+    location.href = '/pages/feed.html';
+  } else {
+    location.href = '/pages/intro.html';
+  }
 }, 1000);
