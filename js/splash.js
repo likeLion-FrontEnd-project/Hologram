0203233c20ed3387a3020a867b27be4d4992046b
GitHWS <gitcodehw@gmail.com>
2022-07-26 10:19:22 +0900

    [Update] 회원가입 / 프로필 수정 페이지 글자수 제한 추가 및 splash 경로 변경 회원가입 / 프로필 수정 페이지 글자수 제한 추가 및 splash 경로 변경
    resolves: #268

--- js/splash.js
+++ js/splash.js
@@ -1,7 +1,7 @@
 // 1초 후 지정한 경로로 페이지 이동
 window.setTimeout(() => {
   if (localStorage.getItem('token')) {
-    location.href = '/pages/feed.html';
+    location.href = '/pages/home.html';
   } else {
     location.href = '/pages/intro.html';
   }
